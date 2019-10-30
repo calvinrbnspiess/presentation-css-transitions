8cf38a7e1c81490639040a0620a07a0ba4189294
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2019-10-30 20:31:14 +0100

    build

---
+++ build/precache-manifest.bdda20d4a717593bc20a82c8b2a8886d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78aa1e57f6ef6b648593",
+    "url": "/static/js/main.78aa1e57.chunk.js"
+  },
+  {
+    "revision": "5a97214ee501a865321e",
+    "url": "/static/css/1.79171df6.chunk.css"
+  },
+  {
+    "revision": "5a97214ee501a865321e",
+    "url": "/static/js/1.5a97214e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0be0f243da17a0e366187f405aa7354f",
+    "url": "/index.html"
+  }
+];
